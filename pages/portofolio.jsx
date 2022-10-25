f09e0611e6e118a0403d150fc5cac3656e39c0b2
aderohmatmaulana98 <aderohmatmaulana77@gmail.com>
2022-10-25 15:21:35 +0700

    image

--- pages/portofolio.jsx
+++ pages/portofolio.jsx
@@ -19,36 +19,36 @@ function Portofolio() {
                         <p className='description'>This is my portfolio .</p>
                         <div className='portofolio-wrapper'>
                             <div className='portofolio-item'>
-                                <Image src="/pusdiklat.png" alt="portofolio-image" className='portofolio-image' />
+                                <img src="/pusdiklat.png" alt="portofolio-image" className='portofolio-image' />
                                 <h4 className='portofolio-name'><Link href='https://github.com/aderohmatmaulana98/pendaftaran-ujian-ept'><a target="_blank" className='git-links'>Sistem Pendaftaran Ujian EPT</a></Link></h4>
                                 <div className='portofolio-category'>Website Dev</div>
                             </div>
                             <div className='portofolio-item'>
-                                <Image src="/sobatutbk.png" alt="portofolio-image" className='portofolio-image' />
+                                <img src="/sobatutbk.png" alt="portofolio-image" className='portofolio-image' />
                                 <h4 className='portofolio-name'><Link href="https://github.com/aderohmatmaulana98/sobatutbk"><a target="_blank" className='git-links'>Sistem Try Out Online</a></Link></h4>
                                 <div className='portofolio-category'>Website Dev</div>
                             </div>
                         </div>
                         <div className='portofolio-wrapper'>
                             <div className='portofolio-item'>
-                                <Image src="/selomerto.png" alt="portofolio-image" className='portofolio-image' />
+                                <img src="/selomerto.png" alt="portofolio-image" className='portofolio-image' />
                                 <h4 className='portofolio-name'><Link href="https://github.com/aderohmatmaulana98/sistem_pembayaran_spp"><a target="_blank" className='git-links'>Sistem Pembayaran SPP dan Buku</a></Link></h4>
                                 <div className='portofolio-category'>Website Dev</div>
                             </div>
                             <div className='portofolio-item'>
-                                <Image src="/pmbakn.png" alt="portofolio-image" className='portofolio-image' />
+                                <img src="/pmbakn.png" alt="portofolio-image" className='portofolio-image' />
                                 <h4 className='portofolio-name'><Link href="https://github.com/aderohmatmaulana98/pmb_akn"><a target="_blank" className='git-links'>Penerimaan Mahasiswa Baru</a></Link></h4>
                                 <div className='portofolio-category'>Website Dev</div>
                             </div>
                         </div>
                         <div className='portofolio-wrapper'>
                             <div className='portofolio-item'>
-                                <Image src="/ikan.png" alt="portofolio-image" className='portofolio-image' />
+                                <img src="/ikan.png" alt="portofolio-image" className='portofolio-image' />
                                 <h4 className='portofolio-name'><Link href="https://github.com/aderohmatmaulana98/sistem-identifikasi-kesegaran-ikan"><a target="_blank" className='git-links'>Sistem Identifikasi Kesegaran Ikan</a></Link></h4>
                                 <div className='portofolio-category'>Machine Learning</div>
                             </div>
                             <div className='portofolio-item'>
-                                <Image src="/kayu.png" alt="portofolio-image" className='portofolio-image' />
+                                <img src="/kayu.png" alt="portofolio-image" className='portofolio-image' />
                                 <h4 className='portofolio-name'><Link href="https://github.com/aderohmatmaulana98/sistem_identifikasi_jenis_kayu"><a target="_blank" className='git-links'>Sistem Identifikasi Jenis Kayu</a></Link></h4>
                                 <div className='portofolio-category'>Machine Learning</div>
                             </div>
